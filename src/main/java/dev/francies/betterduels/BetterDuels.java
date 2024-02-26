70c60bfece19356e72d01877f92c8f68bf264595
Franc1es <ffrferrara@gmail.com>
2024-02-26 16:57:21 +0100

    Commit iniziale

---
+++ src/main/java/dev/francies/betterduels/BetterDuels.java
@@ -0,0 +1,17 @@
+package dev.francies.betterduels;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BetterDuels extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
